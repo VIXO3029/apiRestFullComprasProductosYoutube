7f9727a47f7b0b02f0463275617e479174f42bd8
VIXO3029 <vrodriguez@ilernamadrid.com>
2024-02-04 23:34:40 +0100

    CAMBIOS Y PRUEBAS BACKEND FRONTEND

---
+++ resources/views/nombre/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Listado de Nombres</h1>
+
+        <!-- Tu contenido HTML/Blade aquí -->
+
+        <table>
+            <!-- Mostrar los datos del listado -->
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\NombreController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CompraController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('nombres', NombreController::class);
+
+Route::resource('marcas', MarcaController::class);
+Route::resource('categorias', CategoriaController::class);
+Route::resource('productos', ProductoController::class);
+Route::resource('compras', CompraController::class);
+
+Route::get('categorias/{categoria}/productos', [CategoriaController::class, 'productosPorCategoria']);
+Route::get('marcas/{marca}/productos', [MarcaController::class, 'productosPorMarca']);
